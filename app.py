a3ba80a878d087cd045f9cc3d190ae7b5e83c758
Chris Farley <cfarley95@gmail.com>
2019-06-23 21:46:29 -0400

    getPresenters API endpoint

--- app.py
+++ app.py
@@ -12,3 +12,22 @@ import requests
 
 app = Flask(__name__)
 app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
+
+
+@app.route('/getPresenters', methods=['GET', 'POST'])
+def getPresenters():
+    #data = request.json
+    connection = psycopg2.connect(app.config["DATABASE_URL"])
+    dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    try:
+        with dict_cur as cursor:
+            sql = "SELECT * FROM WHERE presenters;"
+            cursor.execute(sql)
+
+            result = cursor.fetchall()
+            print(result)
+
+            connection.commit()
+    finally:
+        connection.close()
+    return jsonify(result)
