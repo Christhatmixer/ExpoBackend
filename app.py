d6bc1718eede98087c25cb2e45c2bd808f52e8ed
Chris Farley <cfarley95@gmail.com>
2019-07-10 10:17:22 -0400

    fix

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def getPresenterProducts():
 
     try:
         with dict_cur as cursor:
-            sql = "SELECT * FROM products WHERE name = %s"
+            sql = "SELECT * FROM products WHERE creator = %s"
 
             cursor.execute(sql, (data["name"],))
             result = cursor.fetchall()
